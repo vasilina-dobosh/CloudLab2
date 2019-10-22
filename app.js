18f7a9d568d0e03a2e44059a0068e4c70f2a9434
HomeSweetHome <HomeSweetHome@users.noreply.github.com>
2019-10-22 11:04:16 +0700

    [*] Исправлена ошибка не позволяющая запустить приложение

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var app = express();
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+const port = process.env.PORT || 8080;
+
 app.get('/', function (req, res) {
     res.render('index', {title: 'Заголовок', message: 'Тут текст!'});
 });
@@ -11,6 +13,6 @@ app.get('/page3', function (req, res) {
     res.render('page2', {title: 'Страница 2', message: 'Текст второй страницы'});
 });
 
-app.listen(8080, function() {
+app.listen(port, function() {
     console.log('Приложение запущено');
 });
